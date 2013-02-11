44d0e44e531c5c2cac85949e18e79c0896723acc
mxc <matt@curinga.com>
2013-02-11 16:32:59 -0500

    working on adding JS and Nav

---
+++ site/js/xma.js
@@ -0,0 +1,15 @@
+/*
+xma.js
+Version: 0.1
+Author: matthew x. curinga @mxc
+Author URL: http://matt.curinga.com
+Github: https://github.com/mcuringa/xma
+
+*/
+
+
+
+//run this when the page is ready
+$(document).ready(function(){
+    var sequence = $("#welcome").sequence().data("sequence");
+});
