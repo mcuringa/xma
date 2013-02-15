20e900b6710ab59c3ecd717346d6d920e93c0647
mxc <matt@curinga.com>
2013-02-14 22:50:52 -0500

    added more JS to v-align elements

--- site/js/xma.js
+++ site/js/xma.js
@@ -7,17 +7,37 @@ Github: https://github.com/mcuringa/xma
 
 */
 
+//vertically assign elements within vmid
+function vmid()
+{
+    var h = $(window).height();
+    $(".vmid").each(function() {
+        console.log(h)
+        var dif = h -$(this).height();
+        var m = Math.round(dif/2);
+        console.log(m);
+        $(this).css("margin-top", "" + m + "px");});
+}
 
 //dynamically set the section height to be the height of the viewport
 function sectionHeight()
 {
-    $("section").css("min-height", "" + $(window).height());
+    $("section").css("min-height", "" + $(window).height() + "px");
 }
 
+//call the sectionHeight() function when the page loads
+//also, bind it to the window resize event, so that the height
+//is reset if the browser window is resized
 $(function() {
     sectionHeight();
+    vmid();
     $(window).bind('resize', sectionHeight);
+    $(window).bind('resize', vmid);
 });
+
+
+
+
 
 $(function()
 {
